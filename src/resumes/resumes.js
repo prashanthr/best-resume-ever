a14473d4152186178d8225ba757ae17a1f0aeddc
Prashanth <prashanth@slice.com>
2017-09-24 09:52:40 -0700

    feat(resume): Add material dark alternative template

--- src/resumes/resumes.js
+++ src/resumes/resumes.js
@@ -1,5 +1,6 @@
 // If you create a new resume, import it here:
 import '../resumes/material-dark.vue';
+import '../resumes/material-dark-2.vue';
 import '../resumes/left-right.vue';
 import '../resumes/oblique.vue';
 import '../resumes/side-bar.vue';
